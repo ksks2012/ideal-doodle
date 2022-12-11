a7dcfaf4505b461b9eaab48be8259d72356c23c5
ksks2012 <st890751999@gmail.com>
2022-12-11 19:02:38 +0800

    feat(util/job): add Job struct for managing tasks in a distributed computing system

---
+++ util/job.go
@@ -0,0 +1,32 @@
+package util
+
+type JobAction int
+
+const (
+	Map JobAction = iota
+	Reduce
+	Exit
+)
+
+func (jn JobAction) JobNum() string {
+	return [...]string{"Map", "Reduce", "Exit"}[jn]
+}
+
+// TODO: Maybe change to different queue
+type JobState int
+
+const (
+	Waiting JobState = iota
+	Doing
+	Done
+)
+
+func (js JobState) JobState() string {
+	return [...]string{"Waiting", "Doing", "Done"}[js]
+}
+
+type Job struct {
+	Action   JobAction
+	State    JobState
+	FileName string
+}
